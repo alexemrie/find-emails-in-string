6ea0fa156b944c9e350bc5949110a0803939600e
Alex Emrie <emrieaj@gmail.com>
2017-01-20 20:05:12 -0700

    Extra tests for whitespace and terminal punctuation

--- test/email_spec.js
+++ test/email_spec.js
@@ -14,5 +14,29 @@ describe('Emails', function(){
             assert.deepEqual(findEmails(emailSentence), ["multiple@sentence.com", "middle@sentence.com"]);
             assert.deepEqual(findEmails(emailSentence).length, 2);
         });
+
+        it('finds email at end of sentence: period', function() {
+            var emailSentence = "Sentence with end@sentence.com.";
+            assert.deepEqual(findEmails(emailSentence), ["end@sentence.com"]);
+            assert.deepEqual(findEmails(emailSentence).length, 1);
+        });
+
+        it('finds email at end of sentence: question mark', function() {
+            var emailSentence = "Sentence with end@sentence.com?";
+            assert.deepEqual(findEmails(emailSentence), ["end@sentence.com"]);
+            assert.deepEqual(findEmails(emailSentence).length, 1);
+        });
+
+        it('finds email at end of sentence: exclamation point', function() {
+            var emailSentence = "Sentence with end@sentence.com!";
+            assert.deepEqual(findEmails(emailSentence), ["end@sentence.com"]);
+            assert.deepEqual(findEmails(emailSentence).length, 1);
+        });
+
+        it('finds email extra whitespace', function() {
+            var emailSentence = "Sentence with      white@space.com      !";
+            assert.deepEqual(findEmails(emailSentence), ["white@space.com"]);
+            assert.deepEqual(findEmails(emailSentence).length, 1);
+        });
     });
 });
